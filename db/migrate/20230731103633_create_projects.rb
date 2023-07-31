9137a20788cf547b617a1be56bec82fbfe3aaf1b
Moeki Kawakami <me@moeki.dev>
2023-07-31 20:49:28 +0900

    Add project reference to post

--- db/migrate/20230731103633_create_projects.rb
+++ db/migrate/20230731103633_create_projects.rb
@@ -1,6 +1,6 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
-    create_table :projects do |t|
+    create_table :projects, id: :uuid do |t|
       t.string :title, null: false
       t.text :description
       t.string :codename, null: false
