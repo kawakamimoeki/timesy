6386e0f1af6655455170c28d5f1b98a8aba979a5
Moeki Kawakami <me@moeki.dev>
2023-07-31 20:43:36 +0900

    Add project table

---
+++ db/migrate/20230731103633_create_projects.rb
@@ -0,0 +1,14 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.string :title, null: false
+      t.text :description
+      t.string :codename, null: false
+      t.references :user, null: false, foreign_key: true, index: false, type: :uuid
+
+      t.timestamps
+
+      t.index [:user_id, :codename], unique: true
+    end
+  end
+end
