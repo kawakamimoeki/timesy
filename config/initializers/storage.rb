d70a8aca4c663e45232c176a64452c21878810dd
Moeki Kawakami <me@moeki.dev>
2023-08-02 08:27:54 +0900

    Correspond to SVG

---
+++ config/initializers/storage.rb
@@ -0,0 +1,2 @@
+Rails.application.config.active_storage.content_types_to_serve_as_binary.delete("image/svg+xml")
+Rails.application.config.active_storage.content_types_allowed_inline << "image/svg+xml"
