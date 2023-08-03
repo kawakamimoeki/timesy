2f7fcf6a05f30c16a425e118b4597be4978b922f
Moeki Kawakami <me@moeki.dev>
2023-08-03 20:29:30 +0900

    fix timeline

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,6 @@ class PostsController < ApplicationController
 
     all_posts = Post.offset(page_limit*@current_page)
       .includes(:user, comments: :user, post_reactions: :user)
-      .order("comments.created_at DESC")
       .latest
     if current_user
       all_posts = all_posts.following(current_user)
