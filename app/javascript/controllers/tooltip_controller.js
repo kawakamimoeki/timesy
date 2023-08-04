8ae10d07295adfeb441b4e560248855c2e1f3bc2
Moeki Kawakami <me@moeki.dev>
2023-08-04 11:21:16 +0900

    Tweak tooltip style

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -4,9 +4,15 @@ export default class extends Controller {
   static values = { text: String }
 
   connect() {
+    let theme;
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      theme = 'dark';
+    } else {
+      theme = 'light';
+    }
     tippy(this.element, {
       content: this.textValue,
-      theme: 'light',
+      theme: theme,
       animateFill: true,
     });
   }
