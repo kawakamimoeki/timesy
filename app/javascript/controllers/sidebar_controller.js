1469933ca5f296ddc7c9f3a81a489da975f4b187
Moeki Kawakami <me@moeki.dev>
2023-08-08 04:59:27 +0900

    Fix sidebar controller

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -3,7 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect () {
     this.element.addEventListener('click', this.open.bind(this))
-    document.querySelector('#sidebar-close').addEventListener('click', this.close.bind(this))
+    const mo = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.type === "childList") {
+          document.querySelector('#sidebar-close')?.addEventListener('click', this.close.bind(this))
+        }
+      })
+    })
+    mo.observe(document, { childList: true })
+    document.documentElement.addEventListener("turbo:frame-render", () => {
+      document.querySelector('#sidebar-close')?.addEventListener('click', this.close.bind(this))
+    })
   }
 
   open () {
