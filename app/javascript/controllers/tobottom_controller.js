0c0c75e6aaafa74f7e4f0b95253810e198ef0898
Moeki Kawakami <me@moeki.dev>
2023-08-10 00:09:23 +0900

    Hide button when bottom

--- app/javascript/controllers/tobottom_controller.js
+++ app/javascript/controllers/tobottom_controller.js
@@ -3,6 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     this.element.addEventListener("click", this.scroll.bind(this))
+    window.addEventListener("scroll", () => {
+      const bottom = document.body.scrollHeight - window.scrollY - window.innerHeight
+      if (bottom < 100) {
+        this.element.classList.add("hidden")
+      } else {
+        this.element.classList.remove("hidden")
+      }
+    })
   }
 
   scroll() {
