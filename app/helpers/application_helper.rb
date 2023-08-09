ca5654a6411bdeb30292957b8b4d1a3be91454af
Moeki Kawakami <me@moeki.dev>
2023-08-10 08:46:28 +0900

    Fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -67,7 +67,7 @@ module ApplicationHelper
   end
 
   def dropdown_class
-    "overflow-scroll absolute top-10 right-0 z-[100] hidden w-72 bg-white dark:bg-[rgb(30,39,50)] shadow rounded-lg border dark:border-[rgb(48,54,61)] scollbar-none"
+    "overflow-scroll absolute top-10 right-0 z-[100] hidden w-72 bg-white dark:bg-[rgb(30,39,50)] shadow rounded-lg border dark:border-[rgb(48,54,61)] scrollbar-none"
   end
 
   def backdrop_class
