1fe025383c2a3d932847cb2b7fc4de6c1a199863
Moeki Kawakami <me@moeki.dev>
2023-08-11 22:17:42 +0900

    Improve performance

--- app/javascript/controllers/wallpaper_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  static values = { url: String }
-
-  connect() {
-    this.element.style.backgroundImage = `url(${this.urlValue})`
-  }
-}
