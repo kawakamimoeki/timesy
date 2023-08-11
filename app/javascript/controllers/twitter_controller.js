3f7de58b8e25fde78a1ec0698c7b277a4aedb98d
Moeki Kawakami <me@moeki.dev>
2023-08-11 21:59:12 +0900

    Fix twitter card

---
+++ app/javascript/controllers/twitter_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    twttr.widgets.load()
+    const tweets = this.element.querySelectorAll(".twitter-tweet")
+    if (localStorage.theme === 'dark') {
+      if (tweets) {
+        tweets.forEach((tweet) => {
+          tweet.dataset.theme = 'dark'
+        })
+      }
+    }
+  }
+}
