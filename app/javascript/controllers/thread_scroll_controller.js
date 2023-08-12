c36b98a83650580a39b6681bb3b9e5ad24364785
Moeki Kawakami <me@moeki.dev>
2023-08-12 23:09:58 +0900

    Tweak design

--- app/javascript/controllers/thread_scroll_controller.js
+++ app/javascript/controllers/thread_scroll_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect () {
-    mo.observe(this.element, { childList: true })
     document.documentElement.addEventListener("turbo:before-stream-render", (e) => {
       if (e.detail.newStream.getAttribute("target") === "form") {
         this.scroll()
