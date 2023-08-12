cbbbd40f936a154a0a1bfbf02bbc33d5ea0ae195
Moeki Kawakami <me@moeki.dev>
2023-08-12 21:49:14 +0900

    Tweak design

--- app/models/concerns/markdownable.rb
+++ app/models/concerns/markdownable.rb
@@ -4,7 +4,7 @@ module Markdownable
   extend ActiveSupport::Concern
   include ActionView::Helpers::SanitizeHelper
 
-  CACHE_NAMESPACE = "202308121855"
+  CACHE_NAMESPACE = "202308122148"
 
   def truncated(length = 64)
     strip_tags(strip_emoji(html)).gsub(/\n/, " ").gsub(/\//, "").truncate(length)
@@ -32,13 +32,13 @@ module Markdownable
       doc = Nokogiri::HTML::DocumentFragment.parse(markdown.render(body))
       doc.css('pre').each do |pre|
         if pre.children.first.name == "code"
-          pre[:class] = "relative group"
+          pre[:class] = "relative group overflow-hidden"
           pre["data-controller"] = "code-block"
           pre.inner_html = ApplicationController.renderer.render(partial: "shared/code_block", locals: { code: pre.inner_html })
         end
       end
       doc.css('code[@class]').each do |code|
-        code[:class] = "p-4 language-" + code[:class]
+        code[:class] = "language-" + code[:class]
       end
       doc.css('a').each do |link|
         link["data-turbo"] = false
