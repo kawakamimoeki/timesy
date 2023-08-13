72429085ae6bcea0545ea0ffe330db7e9766f9f4
Moeki Kawakami <me@moeki.dev>
2023-08-13 10:36:10 +0900

    Add about site

--- config/routes.rb
+++ config/routes.rb
@@ -48,8 +48,8 @@ Rails.application.routes.draw do
 
   get 'privacy', to: 'pages#privacy', as: :privacy
   get 'terms', to: 'pages#terms', as: :terms
-  get 'about', to: 'pages#about', as: :about
-  get 'docs', to: 'pages#docs', as: :docs
+  get 'about' => redirect('https://about.timesy.dev')
+  get 'docs' => redirect('https://about.timesy.dev/docs')
 
   get '/.well-known/webfinger', to: 'webfinger#index', as: :webfinger
 
