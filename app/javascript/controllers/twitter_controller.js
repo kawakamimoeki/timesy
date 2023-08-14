2a22e7fcc44ce3d460208cac418361906a9c4867
Moeki Kawakami <me@moeki.dev>
2023-08-15 07:58:59 +0900

    Fix

--- app/javascript/controllers/twitter_controller.js
+++ app/javascript/controllers/twitter_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
+    if (!window.twitter.widgets) {
+      return
+    }
     window.twttr.widgets.load()
     const tweets = this.element.querySelectorAll(".twitter-tweet")
     if (localStorage.theme === 'dark') {
