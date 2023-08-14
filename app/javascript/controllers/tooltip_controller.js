39649a7288a8502b61117bb3001dbbcb46bc85fe
Moeki Kawakami <me@moeki.dev>
2023-08-14 12:57:12 +0900

    Revert "Vitefy"
    This reverts commit 34d4d9d4012e68a3b237c4b20c7514f05d4b158e.

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -1,9 +1,4 @@
 import { Controller } from "@hotwired/stimulus"
-import tippy from 'tippy.js'
-import 'tippy.js/dist/tippy.css'
-import 'tippy.js/animations/shift-away.css'
-import 'tippy.js/themes/light.css'
-import 'tippy.js/dist/backdrop.css'
 
 export default class extends Controller {
   static values = { text: String }
--- app/javascript/lib/index.js
+++
@@ -1,2 +0,0 @@
-import "./background.js"
-import "./hljs.js"
