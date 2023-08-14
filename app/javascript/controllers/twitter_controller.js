b8039e5ba8045b7f9d9463e12f59736eeeb072b3
Moeki Kawakami <me@moeki.dev>
2023-08-15 08:03:42 +0900

    Fix

--- app/javascript/controllers/twitter_controller.js
+++ app/javascript/controllers/twitter_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    if (!window.twitter.widgets) {
+    if (!window.twitter?.widgets) {
       return
     }
     window.twttr.widgets.load()
