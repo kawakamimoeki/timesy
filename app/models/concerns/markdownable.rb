0757ccbca1b4831788b96b9ca8b60c64610d5f45
Moeki Kawakami <me@moeki.dev>
2023-08-14 22:35:10 +0900

    Tweak style

--- app/models/concerns/markdownable.rb
+++ app/models/concerns/markdownable.rb
@@ -32,7 +32,7 @@ module Markdownable
       doc = Nokogiri::HTML::DocumentFragment.parse(markdown.render(body))
       doc.css('pre').each do |pre|
         if pre.children.first.name == "code"
-          pre[:class] = "relative group overflow-hidden"
+          pre[:class] = "relative group"
           pre["data-controller"] = "code-block"
           pre.inner_html = ApplicationController.renderer.render(partial: "shared/code_block", locals: { code: pre.inner_html })
         end
