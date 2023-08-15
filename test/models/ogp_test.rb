37d0b409df1193249264a04503e50fc30e5d9237
Moeki Kawakami <me@moeki.dev>
2023-08-16 07:24:54 +0900

    Revert ogp parser

--- test/models/ogp_test.rb
+++ test/models/ogp_test.rb
@@ -3,56 +3,56 @@ require "test_helper"
 class OgpTest < ActiveSupport::TestCase
   test "host" do
     subject = Ogp.new({
-      url: "https://moeki.dev",
+      "url" => "https://moeki.dev",
     })
     assert_equal "moeki.dev", subject.host
   end
 
   test "host with path" do
     subject = Ogp.new({
-      url: "https://moeki.dev/path/to",
+      "url" => "https://moeki.dev/path/to",
     })
     assert_equal "moeki.dev", subject.host
   end
 
   test "host with query" do
     subject = Ogp.new({
-      url: "https://moeki.dev/path/to?query=1",
+      "url" => "https://moeki.dev/path/to?query=1",
     })
     assert_equal "moeki.dev", subject.host
   end
 
   test "host with fragment" do
     subject = Ogp.new({
-      url: "https://moeki.dev/path/to#fragment",
+      "url" => "https://moeki.dev/path/to#fragment",
     })
     assert_equal "moeki.dev", subject.host
   end
 
   test "host with port" do
     subject = Ogp.new({
-      url: "https://moeki.dev:3000",
+      "url" => "https://moeki.dev:3000",
     })
     assert_equal "moeki.dev", subject.host
   end
 
   test "truncate description" do
     subject = Ogp.new({
-      description: "a" * 100,
+      "description" => "a" * 100,
     })
     assert_equal "a" * 61 + "...", subject.truncated_description
   end
 
   test "truncate description with nil" do
     subject = Ogp.new({
-      description: nil,
+      "description" => nil,
     })
     assert_nil subject.truncated_description
   end
 
   test "truncate description with short" do
     subject = Ogp.new({
-      description: "a" * 10,
+      "description" => "a" * 10,
     })
     assert_equal "a" * 10, subject.truncated_description
   end
