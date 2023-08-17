169031413eeb63143d270917c5c355e7eaf98900
Moeki Kawakami <me@moeki.dev>
2023-08-17 21:49:59 +0900

    Add code copy feature

--- app/models/markdown_processor/code.rb
+++ app/models/markdown_processor/code.rb
@@ -2,6 +2,13 @@ class MarkdownProcessor
   class Code
     def self.process(html)
       doc = Nokogiri::HTML::DocumentFragment.parse(html)
+      doc.css('pre').each do |pre|
+        if pre.children.first.name == "code"
+          pre[:class] = "relative group"
+          pre["data-controller"] = "code-block"
+          pre.inner_html = ApplicationController.renderer.render(partial: "shared/code_block", locals: { code: pre.inner_html })
+        end
+      end
       doc.css('code[@class]').each do |code|
         code[:class] = "language-" + code[:class]
       end
