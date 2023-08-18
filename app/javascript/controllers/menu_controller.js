f7a6c9ba505c9f95d1ab74a73586a4adeb0cf596
Moeki Kawakami <me@moeki.dev>
2023-08-18 12:30:30 +0900

    Close menu when page transition

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -3,6 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["dropdown", "backdrop"]
 
+  connect() {
+    this.element.querySelectorAll("a").forEach((link) => {
+      link.addEventListener("click", () => {
+        this.toggle()
+      })
+    })
+  }
+
   toggle() {
     this.dropdownTarget.classList.toggle("hidden")
     this.backdropTarget.classList.toggle("hidden")
