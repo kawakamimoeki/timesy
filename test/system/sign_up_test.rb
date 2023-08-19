04edd9bcb417458067387b7e0e531f82b811e049
Moeki Kawakami <me@moeki.dev>
2023-08-19 22:23:22 +0900

    Improve accessivility

--- test/system/sign_up_test.rb
+++ test/system/sign_up_test.rb
@@ -7,11 +7,11 @@ class SignUpTest < ApplicationSystemTestCase
 
   test 'can prepare to sign up' do
     @page.goto("/")
-    @page.wait_for_selector('[aria-label="Sign up"]').click
+    @page.wait_for_selector('[data-testid="signup-link"]').click
 
-    @page.wait_for_selector('input[aria-label="Email"]').type("new@example.com")
-    @page.wait_for_selector('input[aria-label="Send confirmation email"]').click
+    @page.wait_for_selector('[data-testid="email"]').type("new@example.com")
+    @page.wait_for_selector('[data-testid="submit"]').click
     
-    assert @page.text_content('[aria-label="Notice"]').include?("確認メールを送信しました。メールに記載されたリンクをクリックしてください。")
+    assert @page.wait_for_selector('[role="alert"]').text_content.include?("確認メールを送信しました。メールに記載されたリンクをクリックしてください。")
   end
 end
