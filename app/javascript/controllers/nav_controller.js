20df8df21ec266bdd2beae37dc31cf8ccf5ef7bc
Moeki Kawakami <me@moeki.dev>
2023-08-23 08:58:27 +0900

    Improve nav

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -9,11 +9,18 @@ export default class extends Controller {
         i.classList.add("border-sky-600")
         i.classList.add("dark:border-sky-600")
         i.classList.remove("opacity-30")
+        i.addEventListener("click", this.scroll)
       } else {
         i.classList.remove("border-sky-600")
         i.classList.remove("dark:border-sky-600")
         i.classList.add("opacity-30")
+        i.removeEventListener("click", this.scroll)
       }
     })
+  }
+
+  scroll(e) {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+    e.preventDefault()
   }
 }
