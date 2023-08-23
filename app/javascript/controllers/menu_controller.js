229f948f7fde41dbf7b3da16c1081c59813e7530
Moeki Kawakami <me@moeki.dev>
2023-08-23 09:10:54 +0900

    Fix menu

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -4,22 +4,17 @@ export default class extends Controller {
   static targets = ["dropdown", "backdrop"]
 
   connect() {
-    this.element.querySelectorAll("a").forEach((link) => {
-      link.addEventListener("click", () => {
-        if (this.dropdownTarget.classList.contains("hidden")) {
-          this.dropdownTarget.classList.remove("hidden")
-        } else {
-          this.dropdownTarget.classList.add("hidden")
-        }
-
-        if (this.backdrop.classList.contains("hidden")) {
-          this.backdrop.classList.remove("hidden")
-        } else {
-          this.backdrop.classList.add("hidden")
-        }
-      })
-    })
     this.dropdownTarget.classList.add("hidden")
-    this.backdrop.classList.add("hidden")
+    this.backdropTarget.classList.add("hidden")
+  }
+
+  close() {
+    this.dropdownTarget.classList.add("hidden")
+    this.backdropTarget.classList.add("hidden")
+  }
+
+  open() {
+    this.dropdownTarget.classList.remove("hidden")
+    this.backdropTarget.classList.remove("hidden")
   }
 }
