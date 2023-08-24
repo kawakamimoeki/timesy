6fa8741e852aa1a06b8dc4ab41de51b651c7dd37
Moeki Kawakami <me@moeki.dev>
2023-08-24 17:25:16 +0900

    Fix notificaiton reading api

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class NotificationsController < ApplicationController
+      skip_forgery_protection
+
       def read
         notifications = current_user.notifications.unread
         notifications.update_all(read_at: Time.zone.now)
