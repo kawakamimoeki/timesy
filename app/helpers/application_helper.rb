20ed93cdbde9128cfedd2df3459a53a4c7be2150
Moeki Kawakami <me@moeki.dev>
2023-09-01 18:35:14 +0900

    Tweak design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -71,7 +71,7 @@ module ApplicationHelper
   end
 
   def backdrop_class
-    "hidden fixed top-0 bottom-0 right-0 left-0 inset-0 z-[90] bg-black opacity-0"
+    "hidden fixed top-0 w-screen h-screen left-0 inset-0 z-[90] bg-black opacity-0"
   end
 
   def indicator_class
