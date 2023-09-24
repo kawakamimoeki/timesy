7f0aed9c2ebb0a0c50251e1f6f41a678c7e8fcb9
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2023-09-24 18:28:34 +0900

    fix: Over-Translation of ProjectTag

--- app/models/markdown_processor/project_tag.rb
+++ app/models/markdown_processor/project_tag.rb
@@ -1,14 +1,21 @@
 class MarkdownProcessor
   class ProjectTag
-    def self.process(markdown)
-      markdown.gsub(/#(\w+)/) do |match|
-        project = Project.find_by(codename: $1)
-        if project
-          %(<a href="/#{project.user.username}/projects/#{project.codename}" data-controller="tooltip" data-tooltip-text-value="#{project.title} @#{project.user.username}" data-turbo="false" class="project-tag">#{$&}</a>)
-        else
-          match
+    def self.process(html)
+      doc = Nokogiri::HTML::DocumentFragment.parse(html)
+      doc.css('p').each do |paragraph|
+        paragraph.children.each do |child|
+          next unless child.text?
+          child.replace(child.content.gsub(/#(\w+)/) do |match|
+            project = Project.find_by(codename: $1)
+            if project
+              %(<a href="/#{project.user.username}/projects/#{project.codename}" data-controller="tooltip" data-tooltip-text-value="#{project.title} @#{project.user.username}" data-turbo="false" class="project-tag">#{$&}</a>)
+            else
+              match
+            end
+          end)
         end
       end
+      doc.to_s
     end
   end
 end
--- test/models/markdown_processor/project_tag_test.rb
+++ test/models/markdown_processor/project_tag_test.rb
@@ -3,7 +3,13 @@ require "test_helper"
 class MarkdownProcessor::ProjectTagTest < ActiveSupport::TestCase
   test "returns html with project tag" do
     project = Project.create(codename: "test")
-    result = MarkdownProcessor::ProjectTag.process("#test")
-    assert_equal %(<a href=\"/test/projects/test\" data-controller=\"tooltip\" data-tooltip-text-value=\"test @test\" data-turbo=\"false\" class=\"project-tag\">#test</a>), result
+    result = MarkdownProcessor::ProjectTag.process("<p>#test</p>")
+    assert_equal %(<p><a href="/test/projects/test" data-controller="tooltip" data-tooltip-text-value="test @test" data-turbo="false" class="project-tag">#test</a></p>), result
+  end
+
+  test "not transform when it is not text content" do
+    project = Project.create(codename: "test")
+    result = MarkdownProcessor::ProjectTag.process(%(<p><a href="http://example.com/#test">test</a></p>))
+    assert_equal %(<p><a href="http://example.com/#test">test</a></p>), result
   end
 end
