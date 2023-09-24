abe8a4540a9bd7bd9afdf171205f6106a5b8055b
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2023-09-25 08:38:45 +0900

    Refactor Transform logic for ProjectTag

--- app/models/markdown_processor/project_tag.rb
+++ app/models/markdown_processor/project_tag.rb
@@ -5,17 +5,21 @@ class MarkdownProcessor
       doc.css('p').each do |paragraph|
         paragraph.children.each do |child|
           next unless child.text?
-          child.replace(child.content.gsub(/#(\w+)/) do |match|
-            project = Project.find_by(codename: $1)
-            if project
-              %(<a href="/#{project.user.username}/projects/#{project.codename}" data-controller="tooltip" data-tooltip-text-value="#{project.title} @#{project.user.username}" data-turbo="false" class="project-tag">#{$&}</a>)
-            else
-              match
-            end
-          end)
+          child.replace(transform(child.content))
         end
       end
       doc.to_s
+    end
+
+    def self.transform(text)
+      text.gsub(/#(\w+)/) do |match|
+        project = Project.find_by(codename: $1)
+        if project
+          %(<a href="/#{project.user.username}/projects/#{project.codename}" data-controller="tooltip" data-tooltip-text-value="#{project.title} @#{project.user.username}" data-turbo="false" class="project-tag">#{$&}</a>)
+        else
+          match
+        end
+      end
     end
   end
 end
